8537d66a1ab9577063b43adcf0bcce214ef2dece
shevqnov <sshevqnov@yandex.ru>
2019-07-24 00:55:53 +0500

    implements linked list sorter logic

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
+import { LinkedList } from "./LinkedList";
 import { NumberCollection } from "./NumberCollection";
 import { Sorter } from "./Sorter";
 import { StringCollection } from "./StringCollectioin";
 
 const numbersCollection = new NumberCollection([3, 1, 12, -5, -1, 6]);
 const stringCollection = new StringCollection("Halo");
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(12);
+linkedList.add(-3);
+linkedList.add(0);
 
 const numberSorter = new Sorter(numbersCollection);
 const stringSorter = new Sorter(stringCollection);
+const linkedListSorter = new Sorter(linkedList);
 
 numberSorter.sort();
 stringSorter.sort();
+linkedListSorter.sort();
 
 console.log(numbersCollection.data);
 console.log(stringCollection.data);
+linkedList.print();
